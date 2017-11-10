9025efb90af1376f8f9d8a4740b558193a198445
Y. Luis <y.luis@coodriver.com>
2017-11-10 13:14:16 +0000

    Added Travis files

--- ProducerConsumerProject/settings.py
+++ ProducerConsumerProject/settings.py
@@ -26,7 +26,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'entries',
         'USER': 'root',
-        'PASSWORD': '',
+        'PASSWORD': 'root',
         'HOST': 'localhost',
         'PORT': '3306',
     }
